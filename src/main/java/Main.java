2a07c4fe3bb0f46c00ff358cf3611893c3ed32b2
Josh <joshuastolle445@gmail.com>
2023-05-31 14:27:56 +0200

    lol

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public static String add2(int a, int b) {
+        return "Summe: " + (a + b);
+
+    }
+
+    public static int factorial(int n) {
+        for (int i = n - 1; i > 0; i--) {
+            n *= i;
+        }
+        return n;
+    }
+
+    public static int factorialWhile(int n) {
+        int i = n - 1;
+        while (i > 0) {
+            n *= i;
+            i--;
+        }
+        return n;
+    }
+
+    public static int factorialRecursion(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorialRecursion(n - 1);
+    }
+
+    public static boolean isOneInputBiggerThan100(int a, int b) {
+        return a > 100 || b > 100;
+    }
+
+
+
+    public static void main(String[] args) {
+        int sum = add(1, 2);
+        System.out.println(sum);
+        String sum2 = add2(1, 2);
+        System.out.println(sum2);
+        System.out.println("faculty: " + factorial(5));
+        System.out.println("faculty with while: " + factorialWhile(5));
+        System.out.println("faculty with recursion: " + factorialRecursion(5));
+
+        // Scanner Aufgabe
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie eine Zahl ein und drücken Sie ENTER:");
+        int input1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Geben Sie eine weitere Zahl ein und drücken Sie ENTER:");
+        int input2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Summe: " + add(input1, input2));
+        System.out.println("Ist einer der beiden Werte größer als 100? -> " + isOneInputBiggerThan100(input1, input2));
+    }
+}
